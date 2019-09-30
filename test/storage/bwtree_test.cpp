377abc43af22a0afbc4d72c52807428dda17785e
Matt Butrovich <mbutrovich@gmail.com>
2019-09-30 14:44:26 -0400

    Checkpoint.

--- test/storage/bwtree_test.cpp
+++ test/storage/bwtree_test.cpp
@@ -14,20 +14,39 @@ struct BwTreeTests : public TerrierTest {
 
   void TearDown() override { TerrierTest::TearDown(); }
 
+  const uint32_t num_keys_ = 1e7;
   const uint32_t num_threads_ = 4;
 };
 
 void PrintBasicMemoryStats() { malloc_stats_print(NULL, NULL, NULL); }
 
-/**
- * Adapted from https://github.com/wangziqi2013/BwTree/blob/master/test/iterator_test.cpp
- */
 // NOLINTNEXTLINE
-TEST_F(BwTreeTests, MemoryUsage) {
+TEST_F(BwTreeTests, MemoryUsageSingleThreadInsert) {
+  PrintBasicMemoryStats();
+  auto *const tree = BwTreeTestUtil::GetEmptyTree();
+
+  std::cout << "Allocated: " << tree->GetMemoryUsage() << std::endl;
+
+  for (uint32_t i = 0; i < num_keys_; i++) {
+    BigKey key;
+    key.data_[0] = i;
+    tree->Insert(key, i);
+  }
+
+  PrintBasicMemoryStats();
+
+  std::cout << "Allocated: " << tree->GetMemoryUsage() << std::endl;
+
+  delete tree;
+  PrintBasicMemoryStats();
+}
+
+// NOLINTNEXTLINE
+TEST_F(BwTreeTests, MemoryUsageMultiThreadInsert) {
   PrintBasicMemoryStats();
   auto *const tree = BwTreeTestUtil::GetEmptyTree();
   tree->UpdateThreadLocal(num_threads_ + 1);
-  const uint32_t num_keys = 1000000 * 10;
+  tree->AssignGCID(0);
 
   std::cout << "Allocated: " << tree->GetMemoryUsage() << std::endl;
 
@@ -35,8 +54,8 @@ TEST_F(BwTreeTests, MemoryUsage) {
     const uint32_t gcid = id + 1;
     tree->AssignGCID(gcid);
 
-    uint32_t start_key = num_keys / num_threads_ * id;
-    uint32_t end_key = start_key + num_keys / num_threads_ - 1;
+    uint32_t start_key = num_keys_ / num_threads_ * id;
+    uint32_t end_key = start_key + num_keys_ / num_threads_ - 1;
 
     std::cout << start_key << " " << end_key << std::endl;
 
@@ -48,12 +67,6 @@ TEST_F(BwTreeTests, MemoryUsage) {
     tree->UnregisterThread(gcid);
   };
 
-//  for (uint32_t i = 0; i < num_keys; i++) {
-//    BigKey key;
-//    key.data_[0] = i;
-//    tree->Insert(key, i);
-//  }
-
   common::WorkerPool *thread_pool = new common::WorkerPool(num_threads_, {});
 
   MultiThreadTestUtil::RunThreadsUntilFinish(thread_pool, num_threads_, workload);
@@ -61,6 +74,7 @@ TEST_F(BwTreeTests, MemoryUsage) {
   delete thread_pool;
 
   tree->UpdateThreadLocal(1);
+  tree->AssignGCID(0);
 
   PrintBasicMemoryStats();
 
